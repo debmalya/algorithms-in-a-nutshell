8d0365954d1351864a8815a11f512de889c15375
Eric Jankowski <eric.jankowski@mozido.com>
2015-03-31 09:50:38 -0500

    finished linear addition and test

---
+++ src/main/java/com/erj/practice/misc/LinearAddition.java
@@ -0,0 +1,20 @@
+package com.erj.practice.misc;
+
+public class LinearAddition {
+
+	public static void add(int[] a, int[] b, int[] sum) {
+		int position = a.length-1;
+		int carry = 0;
+		while(position >= 0){
+			int total = a[position] + b[position] + carry;
+			sum[position+1] = total % 10;
+			if (total > 9){
+				carry = 1;
+			}else{
+				carry = 0;
+			}
+			position--;
+		}
+		sum[0] = carry;
+	}
+}
