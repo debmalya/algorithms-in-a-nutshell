68a50e1d0c7654cb2add2ae3365f38090d1de186
Eric Jankowski <eric.jankowski@mozido.com>
2015-04-02 11:16:27 -0500

    finished quadratic multiplication

--- src/main/java/com/erj/practice/misc/QuadraticMultiplication.java
+++ src/main/java/com/erj/practice/misc/QuadraticMultiplication.java
@@ -3,7 +3,19 @@ package com.erj.practice.misc;
 public class QuadraticMultiplication {
 
 	public static void mult(int[] a, int[] b, int[] result) {
-		result[1] = 0;
+		int position = result.length-1;
+		for (int i = 0; i<result.length; i++){
+			result[i] = 0;
+		}
+		for(int m = a.length-1; m >= 0; m--){
+			int offset = a.length-1-m;
+			for(int n = b.length-1; n >= 0; n--, offset++){
+				int product = a[m]*b[n];
+				result[position-offset] += product % 10;
+				result[position-offset-1] += result[position-offset]/10 + product/10;
+				result[position-offset] %= 10;
+			}
+		}
 		
 	}
 
--- src/test/java/com/erj/practice/misc/QuadraticMultiplicationTest.java
+++ src/test/java/com/erj/practice/misc/QuadraticMultiplicationTest.java
@@ -16,5 +16,59 @@ public class QuadraticMultiplicationTest {
 		
 		assertEquals(0, result[1]);
 	}
+	
+	@Test
+	public void addMediumArrays() {
+		int[] a = new int[]{2,2,2};
+		int[] b = new int[]{1,1,1};
+		int[] result = new int[a.length + b.length];
+		
+		QuadraticMultiplication.mult(a, b, result);
+		
+		assertEquals(0, result[0]);
+		assertEquals(2, result[1]);
+		assertEquals(4, result[2]);
+		assertEquals(6, result[3]);
+		assertEquals(4, result[4]);
+		assertEquals(2, result[5]);
+	}
+	
+	@Test
+	public void addBiggerArrays() {
+		int[] a = new int[]{1,2,3,4};
+		int[] b = new int[]{5,6,7,8};
+		int[] result = new int[a.length + b.length];
+		
+		QuadraticMultiplication.mult(a, b, result);
+		
+		assertEquals(0, result[0]);
+		assertEquals(7, result[1]);
+		assertEquals(0, result[2]);
+		assertEquals(0, result[3]);
+		assertEquals(6, result[4]);
+		assertEquals(6, result[5]);
+		assertEquals(5, result[6]);
+		assertEquals(2, result[7]);
+	}
+	
+	@Test
+	public void addEvenBiggerArrays() {
+		int[] a = new int[]{1,2,3,4,5};
+		int[] b = new int[]{6,7,8,9,0};
+		int[] result = new int[a.length + b.length];
+		
+		QuadraticMultiplication.mult(a, b, result);
+		
+		assertEquals(0, result[0]);
+		assertEquals(8, result[1]);
+		assertEquals(3, result[2]);
+		assertEquals(8, result[3]);
+		assertEquals(1, result[4]);
+		assertEquals(0, result[5]);
+		assertEquals(2, result[6]);
+		assertEquals(0, result[7]);
+		assertEquals(5, result[8]);
+		assertEquals(0, result[9]);
+	}
 
 }
