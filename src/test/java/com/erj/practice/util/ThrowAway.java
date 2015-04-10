c0a3fd76cd3e8d18ef581809b8b9ee8c7f216129
Eric Jankowski <eric.jankowski@mozido.com>
2015-04-10 13:49:16 -0500

    Just throwaway code.  I probably shouldn't even be commiting this

---
+++ src/test/java/com/erj/practice/util/ThrowAway.java
@@ -0,0 +1,16 @@
+package com.erj.practice.util;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ThrowAway {
+
+	@Test
+	public void test() {
+		for(int i=0; i<100; i++){
+			System.out.println(100-i);
+		}
+	}
+
+}
